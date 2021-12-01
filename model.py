3ee565cadfb7f752137cfe44b8a81059a3022775
Milad Sadeghi DM <EverLookNeverSee@ProtonMail.ch>
2021-12-01 17:18:13 +0330

    Importing relevant packages

--- model.py
+++ model.py
@@ -3,3 +3,7 @@
     Paper url: https://www.mdpi.com/2076-3417/9/3/404
     Author: Milad Sadeghi DM - EverLookNeverSee@GitHub.com
 """
+
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import (Conv3DTranspose, Conv3D, MaxPooling3D, concatenate, Input)
